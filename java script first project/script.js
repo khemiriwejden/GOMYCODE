ea64d2b5da7914c40883c0f6f4df002693e99f87
khemiriwejden <wejdenekhemiri24@gmail.com>
2026-02-25 09:51:35 +0100

    Add all GOMYCODE projects

---
+++ java script first project/script.js
@@ -0,0 +1,84 @@
+/* IMC 
+var a= Number (prompt("saisi ta taille"))
+var b= Number (prompt("saisi ton poids"))
+var imc = a / (b*b)
+alert ("YOUR IMC : =" + String(imc))  */
+
+/* prog neg ou positive
+var n=Number ( prompt("type a number") )
+if (n>=o){
+alert("positive") }
+else {
+ alert ("negative")
+} */
+
+ /* paire ou impaire
+ var n=Number ( prompt("type a number") )
+if (n % 2 ==0){
+alert("paire") }
+else {
+ alert ("impaire") } */
+
+
+/* + _ ou null
+ var n=Number ( prompt("type a number") )
+if (n>0) {
+alert("positive") }
+else if (n==0) {
+ alert ("null")}
+ else {alert ("negative")} */
+
+ /* season
+ var n=Number ( prompt("type a number") )
+if (n==12 || n==||1 ||n==2){
+alert("winter") }
+else {
+    (n==3 || n==||4 ||n==5){
+alert("spring") }
+else {
+    (n==6 || n==||7 ||n==8){
+alert("summer") }
+else {
+    (n==9 || n==||10 ||n==11){
+alert("automn") }
+else {
+ alert ("invalid") } */
+
+
+
+ /*season  switch 
+  var n=Number ( prompt("type a season number") )
+ switch(n){
+    case 12 :
+    case 1 :
+    case 2 :
+    alert ("winter")
+    break
+    case 3 :
+    case 4 :
+    case 5 :
+    alert ("spring")
+    break
+    case 6 :
+    case 7 :
+    case 8 :
+    alert ("summer")
+    break
+    case 9 :
+    case 10 :
+    case 11 :
+    alert ("automn")
+    break
+    default:
+        alert ("invalid")
+ } */
+
+/* remise sur les prix
+var p =Number (prompt ("type the price"))
+
+if (p > 300){
+p=p*0.9}
+else (p>100)
+{p=p*0.95}
+alert ("final price:" +String(p)) */
+ 
